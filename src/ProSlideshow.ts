287d54588b797d1eff0a3de2a9b74d2d344e2df1
naurisievins <nauris.ievins@gmail.com>
2023-01-27 23:49:39 +0200

    All slideshows finished

---
+++ src/ProSlideshow.ts
@@ -0,0 +1,49 @@
+import AdvancedSlideshow from "./AdvancedSlideshow";
+
+export default class ProSlideshow extends AdvancedSlideshow {
+  fullscreenButton: HTMLDivElement;
+  slideInterval = 10000; // in miliseconds
+  toClearInterval: NodeJS.Timer;
+  
+  constructor(selector: string) {
+    super(selector);
+    this.fullscreenButton = this.rootElement.querySelector('.js-full-screen');
+    
+    this.imageAutoSlide();
+    this.addProListeners();
+  }
+
+  addProListeners() {
+    this.rootElement.addEventListener('click', () => { this.resetIntervalOnClick() })
+    this.fullscreenButton.addEventListener('click', () => { this.openFullscreen() })
+    document.addEventListener("fullscreenchange", () => { this.handleFullscreen() })
+  }
+
+  imageAutoSlide() {
+    this.toClearInterval = setInterval(() => { 
+      this.slideRight();
+      this.markCircle();
+    },
+    this.slideInterval);
+  }
+
+  resetIntervalOnClick() {
+    clearInterval(this.toClearInterval);
+    this.imageAutoSlide();
+  }
+
+  openFullscreen() {
+      this.image.requestFullscreen();
+  }
+
+  handleFullscreen() {
+    if (document.fullscreenElement) {
+      this.fullscreenButton.style.display = 'none'
+      console.log('is full')
+    } else {
+        this.fullscreenButton.style.display = 'block'
+        console.log('exited full')
+    }
+  }
+
+}
--- src/scripts.ts
+++ src/scripts.ts
@@ -1,8 +1,10 @@
 import SimpleSlideshow from './SimpleSlideshow';
 import BasicSlideshow from './BasicSlideshow';
 import AdvancedSlideshow from './AdvancedSlideshow';
+import ProSlideshow from './ProSlideshow';
 
 
 const slideshow1 = new BasicSlideshow('.js-basic-slideshow');
 const slideshow2 = new SimpleSlideshow('.js-simple-slideshow');
 const slideshow3 = new AdvancedSlideshow('.js-advanced-slideshow');
+const slideshow4 = new ProSlideshow('.js-pro-slideshow');
